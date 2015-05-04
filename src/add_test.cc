d5974d59a83a3806030746ae6b5e811098f9849a
limjh <limjh@corelogic.co.kr>
2015-05-05 01:58:12 +0900

    update

--- src/add_test.cc
+++ src/add_test.cc
@@ -8,11 +8,14 @@
 #include "add.h"
 
 TEST(AddTest, onePlusTwoGivesThree) {
+
     EXPECT_EQ(3, add(1, 2));
 }
 
 int main(int argc, char **argv) {
+
     ::testing::InitGoogleTest(&argc, argv);
+
     return RUN_ALL_TESTS();
 }
 
